022330af77ba42ef696cf052c9d673dc620bdd9b
mshroom <32199029+mshroom@users.noreply.github.com>
2017-12-28 19:46:13 +0200

    Update kokeilu.java

--- alik2/kokeilu.java
+++ alik2/kokeilu.java
@@ -1,1 +1,13 @@
-// Leikisti koodia tässä.
+import java.util.Random;
+
+public class Kokeilu {
+  public static void main (String[] aArg) throws Exception  {
+	 	 
+	  for (int i = 0; i < 10; i ++) {
+		  Random arpa = new Random();
+		  int x = arpa.nextInt(3);
+		  int y = arpa.nextInt(3);
+      System.out.println(x + y);
+	  }	 
+  }
+}
